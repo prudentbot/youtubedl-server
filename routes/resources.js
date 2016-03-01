1546ac67921fb079ba2b6aad8fb9fd6e79fa8722
Tyler Smith <tylsmith@gmail.com>
2016-03-01 12:12:43 -0800

    can get search results

--- routes/resources.js
+++ routes/resources.js
@@ -1,4 +1,7 @@
 var express = require('express');
+var request = require('request');
+var secrets = require('../secrets')
+var querystring = require('querystring');
 var router = express.Router();
 
 
@@ -10,9 +13,35 @@ var searchByKeyword = function() {
   }
 }
 
+var base_url = "https://www.googleapis.com/youtube/v3/search?";
+var query = {
+  part: "id,snippet",
+  maxResults:"25",
+  q: "default",
+  key: secrets.YOUTUBE_KEY
+};
+
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
+});
+
+router.get('/:searchstring', function(req, res, next) {
+  query.q = req.params.searchstring;
+  console.log(JSON.stringify(query));
+  var qstring = base_url + querystring.stringify(query);
+  console.log("querying " + qstring);
+  request(qstring, function (error, response, body) {
+    if (!error && response.statusCode == 200) {
+      // console.log(body) // Show the HTML for the Google homepage.
+      res.send(body);
+    }
+    else {
+      res.send(error);
+    }
+  });
+
+  // res.send('resource with arg: ' + req.params.searchstring);
 });
 
 module.exports = router;
