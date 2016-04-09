861de6906f1e2f74904718e375d377fecd7a5ba4
Tyler Smith <tylsmith@gmail.com>
2016-04-09 11:55:46 -0700

    booo

--- routes/resources.js
+++ routes/resources.js
@@ -3,6 +3,7 @@ var request = require('request');
 var secrets = require('../secrets')
 var querystring = require('querystring');
 var youtubeDL = require('youtube-dl');
+var ffmetadata = require("ffmetadata");
 var router = express.Router();
 var fs = require('fs');
 
@@ -24,7 +25,6 @@ router.get('/', function(req, res, next) {
 router.get('/:searchstring', function(req, res, next) {
   query.q = req.params.searchstring;
   var qstring = base_url + querystring.stringify(query);
-  console.log(qstring);
   request(qstring, function (error, response, body) {
     if (!error && response.statusCode == 200) {
       res.send(body);
@@ -37,30 +37,22 @@ router.get('/:searchstring', function(req, res, next) {
 
 router.get('/video/:videoId', function(req, res, next) {
   console.log('requested ' + req.params.videoId);
-  var filename = req.params.videoId + ".mp3";
-  var title = "placeholder";
-  youtubeDL.getInfo(req.params.videoId, [], function(err, info) {
-    if (err) {
-      res.send("bad request");
-      return;
-    }
-    title = info.title;
-  });
 
-  youtubeDL.exec(req.params.videoId, ['-x', '--audio-format', 'mp3', '-o', filename], {}, function(err, output) {
+  youtubeDL.exec(req.params.videoId, ['--add-metadata', '-x', '--audio-format', 'mp3'], {}, function(err, output) {
     if (err){
       res.send("bad request");
       return;
     }else{
-      console.log("success!")
+      var outputString = output.toString();
+      var filenamei = outputString.search("Destination: .*.mp3");
+      var endi = outputString.search("mp3");
+      var filename = outputString.substring(filenamei + 13, endi + 3);
       res.setHeader("content-type", "audio/mp3");
-      console.log(title);
-      res.setHeader("song-title", title);
-      fs.createReadStream(filename).pipe(res);
-      fs.unlink(filename);
+      fs.createReadStream(filename).pipe(res).on('close', function() {
+        fs.unlink(filename);
+      }).pipe(res);
     }
   });
-
 });
 
 module.exports = router;
